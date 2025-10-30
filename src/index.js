dfa509347e4d03e4e8f9a964aef50176b5af3d33
tiep.hoangpham@imipgroup.com <tiephoang.dev@gmail.com>
2025-10-30 10:18:04 +0700

    fix a recursive dependency

--- src/index.js
+++ src/index.js
@@ -1477,7 +1477,7 @@ export default class Gantt {
             }, []);
 
             out = out.concat(deps);
-            to_process = deps.filter((d) => !to_process.includes(d));
+            to_process = deps.filter((d) => !to_process.includes(d) && !out.includes(d));
         }
 
         return out.filter(Boolean);
